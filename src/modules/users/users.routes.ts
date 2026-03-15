8e853de323e84b69b8f53b8bd18fb679f354c5ce
Fera <feraferaca@gmail.com>
2026-03-15 19:01:54 +0300

    docs: add openapi to users routes

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -3,10 +3,112 @@ import { UserController } from "./users.controller.js";
 
 const router = Router();
 
+/**
+ * @openapi
+ * /users:
+ *   post:
+ *     summary: Create user
+ *     description: Creates a user directly through users module
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateUserRequest'
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/SafeUser'
+ *       400:
+ *         $ref: '#/components/responses/ValidationError'
+ *       409:
+ *         $ref: '#/components/responses/ConflictError'
+ *       500:
+ *         $ref: '#/components/responses/InternalError'
+ */
 router.post("/", UserController.create);
 
+/**
+ * @openapi
+ * /users:
+ *   get:
+ *     summary: Get all users
+ *     description: Returns a list of all users without passwordHash
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: List of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/SafeUser'
+ *       500:
+ *         $ref: '#/components/responses/InternalError'
+ */
 router.get("/", UserController.list);
+
+/**
+ * @openapi
+ * /users/{id}/tasks:
+ *   get:
+ *     summary: Get tasks of a user by id
+ *     description: Returns all tasks of a specific user
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User id
+ *     responses:
+ *       200:
+ *         description: List of user tasks
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Task'
+ *       404:
+ *         $ref: '#/components/responses/NotFoundError'
+ *       500:
+ *         $ref: '#/components/responses/InternalError'
+ */
 router.get("/:id/tasks", UserController.getTasks);
+
+/**
+ * @openapi
+ * /users/{id}:
+ *   get:
+ *     summary: Get user by id
+ *     description: Returns a single user without passwordHash
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User id
+ *     responses:
+ *       200:
+ *         description: User found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/SafeUser'
+ *       404:
+ *         $ref: '#/components/responses/NotFoundError'
+ *       500:
+ *         $ref: '#/components/responses/InternalError'
+ */
 router.get("/:id", UserController.getUser);
 
 export default router;
