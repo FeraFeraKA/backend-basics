54e4efe84a78d171c9767021694ba0ed5df898c2
Fera <feraferaca@gmail.com>
2026-03-21 15:13:13 +0300

    chore: add env example

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import app from "./app.js";
 
-const port = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
